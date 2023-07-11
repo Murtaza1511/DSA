ac1cbb851cb7d101d71a5531620a088aab05d4f4
Murtaza Tinwala <murtazatinwala1511@gmail.com>
2023-07-11 20:48:28 +0530

    removed package strings

--- Strings/Strings_01_FreqOfCharacters.java
+++ Strings/Strings_01_FreqOfCharacters.java
@@ -1,9 +1,13 @@
-package Strings;
+
 // -Sequence of Characters
 // -Smaller set of values i.e. Ascii about 128 values
 // -Every character is given an integer value to it 
 // -Java uses UTF-16 to store characters but it remains same for
 // 1st 128 ASCII characters
+// geeksforgeeks
+// e 5
+// f 3
+
 public class Strings_01_FreqOfCharacters {
     public static void main(String[] args) 
     {
